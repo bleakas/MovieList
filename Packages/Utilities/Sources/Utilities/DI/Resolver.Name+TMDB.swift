85ac87699e20e26aae84e0d45edb92157aa59d5e
Pavel <pavelshutkou@Pavels-MacBook-Pro.local>
2023-02-03 22:19:06 +0400

    Added TMDB named dependencies for Utilities package

---
+++ Packages/Utilities/Sources/Utilities/DI/Resolver.Name+TMDB.swift
@@ -0,0 +1,12 @@
+//
+//  Resolver.Name+TMDB.swift
+//  
+//
+//  Created by Pavel Shutkou on 03.02.23.
+//
+
+import Resolver
+
+public extension Resolver.Name {
+    static let TMDBApiClient = Self("TMDBApiClient")
+}
