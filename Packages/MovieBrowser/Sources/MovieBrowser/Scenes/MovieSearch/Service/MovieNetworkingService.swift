323239642976c7b0299db8173645332e0627df72
Pavel <pavelshutkou@Pavels-MacBook-Pro.local>
2023-02-05 00:46:53 +0400

    Added implementation for MovieNetworkService

--- Packages/MovieBrowser/Sources/MovieBrowser/Scenes/MovieSearch/Service/MovieNetworkingService.swift
+++ Packages/MovieBrowser/Sources/MovieBrowser/Scenes/MovieSearch/Service/MovieNetworkingService.swift
@@ -5,6 +5,7 @@
 //  Created by Pavel Shutkou on 03.02.23.
 //
 
+import Foundation
 import Resolver
 import Utilities
 import TMDBClient
@@ -12,15 +13,39 @@ import Get
 
 public final class MovieNetworkingService {
     @Injected(name: .TMDBApiClient) private var apiClient: APIClient
+    private let language = Locale.current.identifier
     public init() {}
 }
 
 extension MovieNetworkingService: MovieServiceProtocol {
     public func getPopularMovies(page: Int) async throws -> [MovieDetails] {
-        return []
+        let request = Paths.movie.popular.get(page: page, language: language)
+        return try await extractMoviesFromMovieList(request: request)
     }
 
     public func searchMovies(query: String, page: Int) async throws -> [MovieDetails] {
-        return []
+        let parameters = Paths.Search.Movie.GetParameters(page: page, language: language, query: query)
+        let request = Paths.search.movie.get(parameters: parameters)
+        return try await extractMoviesFromMovieList(request: request)
+    }
+
+    private func extractMoviesFromMovieList(request: Request<TMDBClient.MovieList>) async throws -> [MovieDetails] {
+        let popular = try await apiClient.send(request)
+        let movieIDs = (popular.value.results ?? []).map {
+            $0.id
+        }
+        return try await withThrowingTaskGroup(of: MovieDetails.self, returning: [MovieDetails].self) { group in
+            for id in movieIDs {
+                group.addTask {
+                    let request = Paths.movie.movieID(id).get(appendToResponse: .credits)
+                    return try await self.apiClient.send(request).value
+                }
+            }
+            var movies = [MovieDetails]()
+            for try await item in group {
+                movies.append(item)
+            }
+            return movies
+        }
     }
 }
