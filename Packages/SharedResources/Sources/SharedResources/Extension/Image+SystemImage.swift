d8b83b41e2cac29d5fbc804f0f6206686d267677
Pavel <pavelshutkou@Pavels-MacBook-Pro.local>
2023-02-07 22:19:28 +0400

    Implemented extension for system images in SharedResources package

---
+++ Packages/SharedResources/Sources/SharedResources/Extension/Image+SystemImage.swift
@@ -0,0 +1,12 @@
+//
+//  Image+SystemImage.swift
+//  
+//
+//  Created by Pavel Shutkou on 07.02.23.
+//
+
+import SwiftUI
+
+public extension Image {
+    static var chevronForward = Image(systemName: "chevron.forward")
+}
